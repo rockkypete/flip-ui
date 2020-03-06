d91e8dfe85beabfc3bfd61be05a831325442dc15
Peter Abah <abahp22@gmail.com>
2020-03-06 08:54:15 +0100

    modified custom_script for random cnd usage

--- static/js/custom_script.js
+++ static/js/custom_script.js
@@ -1,26 +1,38 @@
+//async function (promise) to force browser delay
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 //custom object for theme request handle
 const themeRequestObj = {
     localSource: '/themes',
     extSource: 'https://bootswatch.com/api/4.json',
     servedData: {},
-    ajaxTheme : function (source) {
+    ajaxTheme : function (source, attrib) {
         $.ajax({
             method: 'GET',
             url: `${ source }`,
             dataType: 'json'
         }).done((data)=> {
             this.servedData = data;
-            console.log(this.servedData);
+            let themes = themeRequestObj.servedData.themes;
+            let theme = themes[Math.floor(Math.random()* themes.length)]
+            console.log(`you selected ${ theme.name } with a cdn of ${ theme[attrib] }`);   
+            
+                    //$('head link:first').attr('href', `${theme[attrib]}`);                 
+        
         });
     },
-    fetchTheme: function(source){
+    fetchTheme: function(source, attrib){
         fetch(`${ source }`)
         .then((res)=> res.json())
         .then((data)=> {
             this.servedData = data;
-            console.log(this.servedData);
-        });
+            let themes = themeRequestObj.servedData.themes;
+            let theme = themes[Math.floor(Math.random()* themes.length)]
+            console.log(`you selected ${ theme.name } with a cdn of ${ theme[attrib] }`);            
+        })
     }
 }
 
@@ -28,60 +40,44 @@ $('#Ajax-btn').click((e)=> {
     //prevent form submmision since we're not persisting to db
     e.preventDefault();
     //ajax theme from local theme.json file
-    themeRequestObj.ajaxTheme(themeRequestObj.localSource);
-    let themes = themeRequestObj.servedData.themes;
-    $('head link:first').attr('href', `${ themes[0].cdn }`)      
+    themeRequestObj.ajaxTheme(themeRequestObj.localSource, 'cdn');
 })
 
 $('#Ajax-api').click((e)=> {
-    //prevent form submmision since we're not persisting to db
     e.preventDefault();
 
     //ajax theme from bootswatch api
-    themeRequestObj.ajaxTheme('https://bootswatch.com/api/4.json');
-    let themesArray = themeRequestObj.servedData.themes;
-    $('head link:first').attr('href', `${ themesArray[3].cssCdn }`);
+    themeRequestObj.ajaxTheme(themeRequestObj.extSource, 'cssCdn');
 });
 
 $('#Fetch-btn').click((e)=> {
-    //prevent form submmision since we're not persisting to db
     e.preventDefault();
+
     //fetch theme from local theme.json file
-    themeRequestObj.fetchTheme(themeRequestObj.localSource);
-    let themes = themeRequestObj.servedData.themes;
-    $.each(themes, (i, theme) => {
-        console.log(`you selected ${ theme.name } with a cdn of ${ theme.cdn }`);  
-    });
+    themeRequestObj.fetchTheme(themeRequestObj.localSource, 'cdn');
 });
        
 $('#Fetch-api').click((e)=> {
-    //prevent form submmision since we're not persisting to db
     e.preventDefault();
 
     //fetch theme from bootswatch api
-    themeRequestObj.fetchTheme(themeRequestObj.extSource);
-    let themesArray = themeRequestObj.servedData.themes;
-    alert(themesArray);
-    $,each(themesArray, (i, theme)=>{
-        let attribute = $('head link:first').attr('href');
-        console.log(attribute);
-    });
+    themeRequestObj.fetchTheme(themeRequestObj.extSource, 'cssCdn');
 });
 
 //select onchange logic
 $('select#theme').change((e)=> {
-    themeRequestObj.ajaxTheme(themeRequestObj.localSource);
-})
-
-//async function (promise) to force browser delay
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-/*
- wait 2seconds then reload the current page on skin button click
-    sleep(2000).then(()=> {
-        location.reload();
-        alert(`${ uiDesign } theme is now active!`);     
+    let themeChoice = e.target.value; 
+    $.ajax({
+        method: 'GET',
+        url: `${ themeRequestObj.localSource }`,
+        dataType: 'json'
+    }).done((data)=> {
+        let themeObjList = data.themes;
+        $.each(themeObjList, (i, theme)=>{
+            if (theme.name === themeChoice){
+                //$('head link:first').attr('href', `${theme.cdn}`);
+                console.log(`You selected ${theme.name} with a cdn of ${theme.cdn}`)
+            }
+        });
     });
-*/
+});
